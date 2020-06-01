c0d164999ef80893d6ab851cd28a248b7bbd4b7a
Omeljan Konstantin <kos.omeljan@gmail.com>
2020-06-01 08:55:42 +0300

    Update auto_ria_parser.py

--- auto_ria_parser.py
+++ auto_ria_parser.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 URL = 'https://auto.ria.com/newauto/marka-jeep/'
+#Windows
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
 			 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 '''
